459a2742e550d1ef8d7e7117c73a96404e4769ef
Carlo Marcello <carlo.marcello@gmail.com>
2019-03-22 22:07:12 -0300

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {    	
+      valor: 1
+    }
+  }
+
+  incrementaContador() {
+    const novoValor = this.state.valor + 1;
+
+    this.setState({
+      valor: novoValor
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App!</h1>
+        <button onClick={this.incrementaContador.bind(this)}>Incrementa contador</button>        
+        <p>Contador: {this.state.valor}</p>
+
+        <Person name="Carlo" />
+        <Person name="Sara" />
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const person = (props) => {
+    return(
+        <div>O nome é {props.name}</div>
+    );
+}
+
+export default person;
